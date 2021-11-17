f7e4ba03b4fa443fb06ca6f04333edcbe3116c6a
sephson <disutj@gmail.com>
2021-11-17 18:50:43 +0100

    added loading to home page

--- jiji-client/src/components/Trending/Trending.js
+++ jiji-client/src/components/Trending/Trending.js
@@ -7,7 +7,7 @@ import { items } from "../../actions/sell.action";
 const Trending = () => {
   const dispatch = useDispatch();
   const a = useSelector((state) => state.allItems);
-  const { allItems } = a;
+  const { loading, allItems } = a;
 
   useEffect(() => {
     dispatch(items());
@@ -19,17 +19,30 @@ const Trending = () => {
 
   return (
     <div className="trends">
-      {allItems?.all.map((trend) => {
-        return (
-          <div className="trend-container">
-            <Link to={`/item/${trend._id}`}>
-              <img className="trend-image" src={trend.image} alt="trend art" />
-              <p className="trend-text">{trend.name}</p>
-              <p className="trend-price">₦{numberWithCommas(trend.price)}</p>
-            </Link>
-          </div>
-        );
-      })}
+      {loading ? (
+        <ReactLoading
+          type={"bars"}
+          color={"#3db83a"}
+          height={"10%"}
+          width={"10%"}
+        />
+      ) : (
+        allItems?.all.map((trend) => {
+          return (
+            <div className="trend-container">
+              <Link to={`/item/${trend._id}`}>
+                <img
+                  className="trend-image"
+                  src={trend.image}
+                  alt="trend art"
+                />
+                <p className="trend-text">{trend.name}</p>
+                <p className="trend-price">₦{numberWithCommas(trend.price)}</p>
+              </Link>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
