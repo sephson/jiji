9fb8e02259e9361ec87ff9dbf3f858fe37818687
sephson <disutj@gmail.com>
2021-11-17 13:02:44 +0100

    app error fix heroku

--- server/index.js
+++ server/index.js
@@ -38,4 +38,4 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => console.log(`Server is running on 5000`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server is running on 5000`));
