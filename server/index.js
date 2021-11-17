2dd527c6a7fe5e6ef8cbc328cd8f1d5ae0ee6bc4
sephson <disutj@gmail.com>
2021-11-17 13:46:18 +0100

    api production malfunction possible fix

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../jiji-client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../jiji-client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../jiji-client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
